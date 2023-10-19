c465de351ec4ba73204b4686f1ff19ece76928cf
Kurtleo <kdomena47@gmail.com>
2023-10-19 14:06:55 -0400

    7-print_tebahpla.c

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = z; n < a; n++)
+	putchar(n + 'z');
+	putchar('\n');
+
+	return (z)
+
+}
