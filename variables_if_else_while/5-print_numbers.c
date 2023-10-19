0d9e63e59379663e01ae371ff41bd22f221762c6
Kurtleo <kdomena47@gmail.com>
2023-10-19 11:53:31 -0400

    5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+/*
+ *
+ * File: 5-Print_numbers.c
+ * Auth: Kurt Domena
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int 11;
+
+	for (11 = 0; 11 < 10 11;++)
+		printf("%d", 11);
+
+	printf("\n");
+
+	return (0);
+}
