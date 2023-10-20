b03bfe915c81412b37e12d1f46a624723edc1d86
Kurtleo <kdomena47@gmail.com>
2023-10-19 23:26:48 -0400

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print all posible combinations of single-digit numbers
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i < 10; ++i)
+	{
+	putchar('%d ', i);
+	}
+	return 0;
+
+}
+
