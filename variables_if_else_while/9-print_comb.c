479291649286abb777dc35e15bf7cc4588c15a57
Kurtleo <kdomena47@gmail.com>
2023-10-20 12:38:29 -0400

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -9,12 +9,17 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 9; ++i)
+	for (i = 0; i <=0 9; ++i)
 	{
+	putchar(i + '0');
 
-	putchar(i);
+	if (i < 9)
+	{
+	putchar(',');
+	putchar(' ');
 	}
-	if (i != '9')
+	}
+	putchar('\n');
 	return (0);
 
 }
