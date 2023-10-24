8fbd77858101f1d3fdb443b6cb4e38c2c8138e31
Kurtleo <kdomena47@gmail.com>
2023-10-24 14:33:46 -0400

    1-swaps.c

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * swap_int -Write a function that swaps the values of two
+ *
+ * @a: An integer input pointer
+ * @b: An integer input pointer
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 98;
+	*b = 42;
+}
