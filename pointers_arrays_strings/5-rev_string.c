7b4e7c48b4d99e738e8f3557c5988a65670fdbaf
Kurtleo <kdomena47@gmail.com>
2023-10-26 14:17:25 -0400

    5-rev_string.c

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -15,8 +15,9 @@ void rev_string(char *s)
 
 	for (index = len - 1; index >= len / 2; index--)
 	{
-		tmp = s [index];
-		s [index] = s[len - index -1];
+	tmp = s [index];
+	s [index] = s[len - index -1];
+	:wq
 		s[len - index - 1] = tmp;
 	}
 }
